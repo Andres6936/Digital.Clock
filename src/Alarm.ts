c93c74e61ab0e53b019e08d457d129470e8dc202
Andres6936 <andres6936@gmail.com>
2023-04-25 12:09:09 -0500

    Added Style

--- src/Alarm.ts
+++ src/Alarm.ts
@@ -7,6 +7,19 @@ class Alarm extends LitElement {
     div {
         background:url('/img/alarm_light.jpg');
     }
+
+    div {
+        width:16px;
+        height:16px;
+        bottom:20px;
+        background:url('/img/alarm_light.jpg');
+        position:absolute;
+        opacity:0.2;
+    }
+
+    div.active {
+        opacity:1;
+    }
     `
 
     render() {
