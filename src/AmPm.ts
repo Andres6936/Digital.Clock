b825be452e227bf45f089be52355dd40b3b0e924
Andres6936 <andres6936@gmail.com>
2023-04-25 12:15:24 -0500

    Added Style

--- src/AmPm.ts
+++ src/AmPm.ts
@@ -1,8 +1,17 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement} from 'lit/decorators.js';
 
 @customElement('digital-ampm')
 class AmPm extends LitElement {
+    static styles = css`
+    div {
+        position:absolute;
+        bottom:20px;
+        right:20px;
+        font-size:12px;
+    }
+    `
+
     render() {
         return html`
             <div class="ampm"></div>
--- src/Weekdays.ts
+++ src/Weekdays.ts
@@ -1,8 +1,28 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, css} from 'lit';
 import {customElement} from 'lit/decorators.js';
 
 @customElement('digital-weekdays')
 class Weekdays extends LitElement {
+    static styles = css`
+    div {
+        font-size:12px;
+        position:absolute;
+        width:100%;
+        top:10px;
+        left:0;
+        text-align:center;
+    }
+
+    div span{
+        opacity:0.2;
+        padding:0 10px;
+    }
+
+    div span.active{
+        opacity:1;
+    }
+    `
+
     render() {
         return html`
             <div class="weekdays"></div>
