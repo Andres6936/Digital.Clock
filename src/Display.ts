dd3a443c235952576d4980cbd0b5524dc501d885
Andres6936 <andres6936@gmail.com>
2024-08-17 09:26:37 -0500

    Refactor theme handling in Display component
    Moved theme-specific styles to CSS variables and applied them using class-based approach. Simplified the render method by removing the getThemeStyle function, reducing redundancy and improving code clarity.

--- src/Display.ts
+++ src/Display.ts
@@ -9,46 +9,33 @@ import './TableDigits.ts'
 @customElement('digital-display')
 class Display extends LitElement {
     static styles = css`
-    div {
-        text-align:center;
-        padding: 40px 20px 20px;
-        border-radius:6px;
-        position:relative;
-        height: 54px;
-    }
-
-
+        .light {
+            --theme-display-background-color: #dddddd;
+            --theme-display-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08) inset, 0 1px 1px #fafafa;
+        }
+        
+        .dark {
+            --theme-display-background-color: #0f1620;
+            --theme-display-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08) inset, 0 1px 1px #2d3642;
+        }
+        
+        div {
+            background-color: var(--theme-display-background-color);
+            box-shadow: var(--theme-display-box-shadow);
+            text-align: center;
+            padding: 40px 20px 20px;
+            border-radius: 6px;
+            position: relative;
+            height: 54px;
+        }
     `
 
     @property({type: String})
     theme!: string
 
-    getThemeStyle() {
-        if (this.theme === "dark") {
-            return html`
-                <style>
-                    div {
-                        background-color: #0f1620;
-                        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08) inset, 0 1px 1px #2d3642;
-                    }
-                </style>
-            `
-        } else {
-            return html`
-                <style>
-                    div {
-                        background-color: #dddddd;
-                        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08) inset, 0 1px 1px #fafafa;
-                    }
-                </style>
-            `
-        }
-    }
-
     render() {
         return html`
-            ${this.getThemeStyle()}
-            <div class="display">
+            <div class="${"display " + this.theme}">
                 <digital-weekdays></digital-weekdays>
                 <digital-ampm></digital-ampm>
                 <digital-alarm theme="${this.theme}"></digital-alarm>
