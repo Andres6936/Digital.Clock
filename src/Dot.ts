98b0b58baca0e35730be01e67a9fa9e845e621e6
Andres6936 <andres6936@gmail.com>
2024-08-17 10:15:51 -0500

    Refactor theme handling in Dot component
    Removed inline theme styles and added CSS class-based theme handling. This improves maintainability by centralizing the theme logic within the CSS, making it easier to modify or expand themes in the future. Simplified the render method by directly applying the theme class.

--- src/Dot.ts
+++ src/Dot.ts
@@ -4,63 +4,49 @@ import {customElement, property} from 'lit/decorators.js';
 @customElement('digital-dot')
 class Dot extends LitElement {
     static styles = css`
-     div {
-        text-align:left;
-        position:relative;
-        width: 28px;
-        height:50px;
-        display:inline-block;
-        margin:0 4px;
-    }
+        .light {
+            --theme-dot-background-color: #272e38;
+        }
 
-     div.dots{
-        width:5px;
-    }
+        .dark {
+            --theme-dot-background-color: #cacaca;
+        }
 
-     div.dots:before,
-     div.dots:after{
-        width:5px;
-        height:5px;
-        content:'';
-        position:absolute;
-        left:0;
-        top:14px;
-    }
+        div {
+            text-align: left;
+            position: relative;
+            width: 28px;
+            height: 50px;
+            display: inline-block;
+            margin: 0 4px;
+        }
 
-     div.dots:after{
-        top:34px;
-    }
+        div.dots {
+            width: 5px;
+        }
+
+        div.dots:before,
+        div.dots:after {
+            width: 5px;
+            height: 5px;
+            content: '';
+            position: absolute;
+            left: 0;
+            top: 14px;
+            background-color: var(--theme-dot-background-color);
+        }
+
+        div.dots:after {
+            top: 34px;
+        }
     `
 
     @property({type: String})
     theme!: string
 
-    getThemeStyle() {
-        if (this.theme === "dark") {
-            return html`
-                <style>
-                    div.dots:before,
-                    div.dots:after {
-                        background-color: #cacaca;
-                    }
-                </style>
-            `
-        } else {
-            return html`
-                <style>
-                    div.dots:before,
-                    div.dots:after {
-                        background-color: #272e38;
-                    }
-                </style>
-            `
-        }
-    }
-
     render() {
         return html`
-            ${this.getThemeStyle()}
-            <div class="dots"></div>
+            <div class="dots ${this.theme}"></div>
         `;
     }
 }
