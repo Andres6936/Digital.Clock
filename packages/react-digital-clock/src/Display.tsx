0df0f592af21f6df56d177d80e97fcfccf476e47
Andres6936 <andres6936@gmail.com>
2025-01-17 12:08:39 -0500

    Simplify theme handling by replacing CSS classes.
    Replaced the theme-specific CSS classes for light and dark modes with the `light-dark` utility function. This reduces redundancy and enhances maintainability of the code. The `Container` no longer requires a specific class for theme application.

--- packages/react-digital-clock/src/Display.tsx
+++ packages/react-digital-clock/src/Display.tsx
@@ -3,30 +3,18 @@ import {TableDigits} from "./TableDigits";
 import styled from "styled-components";
 
 const Container = styled.div`
-    .light {
-        --theme-display-background-color: #dddddd;
-        --theme-display-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08) inset, 0 1px 1px #fafafa;
-    }
-
-    .dark {
-        --theme-display-background-color: #0f1620;
-        --theme-display-box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08) inset, 0 1px 1px #2d3642;
-    }
-
-
-    background-color: var(--theme-display-background-color);
-    box-shadow: var(--theme-display-box-shadow);
+    background-color: light-dark(#dddddd, #0f1620);
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.08) inset, 0 1px 1px ligth-dark(#fafafa, #2d3642);
     text-align: center;
     padding: 40px 20px 20px;
     border-radius: 6px;
     position: relative;
     height: 54px;
-
 `
 
 export function Display() {
     return (
-        <Container className="light">
+        <Container>
             <Weekdays/>
             <AmPm/>
             <Alarm/>
